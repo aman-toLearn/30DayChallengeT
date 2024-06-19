6b94a205b1659ca5ccc98d666e5552b3c776a4c6
aman8094 <realaman09@gmail.com>
2024-06-19 16:41:17 +0530

    dayone

--- src/DayOne.java
+++ src/DayOne.java
@@ -1,2 +1,20 @@
+
+
+
+//Question:
+//        Write a program that checks whether a given integer value n ends with 0.
+//
+//        Example:
+//        Input: 1230
+//        Output: Yes
 public class DayOne {
+    public static void main(String[] args) {
+        int N=1230;
+        if (N % 10 == 0) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
 }
